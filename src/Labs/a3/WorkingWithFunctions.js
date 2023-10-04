bfa1b0e7ab3aa3fde379d29d107d68bf655e508c
caocynthia <cynthiacao.work@gmail.com>
2023-10-04 17:04:39 -0400

    lab

---
+++ src/Labs/a3/WorkingWithFunctions.js
@@ -0,0 +1,34 @@
+import ES5Functions from "./ES5Functions";
+import ArrowFunctions from "./ArrowFunctions";
+import ImpliedReturns from "./ImpliedReturns";
+import FunctionParenthesisAndParameters from "./FunctionParenthesisAndParameters";
+import WorkingWithArrays from "./WorkingWithArrays";
+import ArrayIndexAndLength from "./ArrayIndexAndLength";
+import AddingAndRemovingDataToFromArrays from "./AddingAndRemovingDataToFromArrays";
+import ForLoops from "./ForLoops";
+import MapFunction from "./MapFunction";
+import JsonStringify from "./JsonStringify";
+import FindFunction from "./FindFunction";
+import FindIndex from "./FindIndex";
+import FilterFunction from "./FilterFunction";
+
+function WorkingWithFunctions() {
+  return (
+    <div>
+      <ES5Functions />
+      <ArrowFunctions />
+      <ImpliedReturns />
+      <FunctionParenthesisAndParameters />
+      <WorkingWithArrays />
+      <ArrayIndexAndLength />
+      <AddingAndRemovingDataToFromArrays />
+      <ForLoops />
+      <MapFunction />
+      <JsonStringify />
+      <FindFunction />
+      <FindIndex />
+      <FilterFunction />
+    </div>
+  );
+}
+export default WorkingWithFunctions;
