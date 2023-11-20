66529954a14edfa5d7dade6ba010e0000ceed98f
caocynthia <cynthiacao.work@gmail.com>
2023-11-19 20:33:57 -0500

    working with arrays

--- src/Labs/a5/EncodingParametersInURLs.js
+++ src/Labs/a5/EncodingParametersInURLs.js
@@ -1,10 +1,39 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+
 function EncodingParametersInURLs() {
   const [a, setA] = useState(34);
   const [b, setB] = useState(23);
+  const [welcome, setWelcome] = useState("");
+
+  const fetchWelcome = async () => {
+    const response = await axios.get("http://localhost:4000/a5/welcome");
+    setWelcome(response.data);
+  };
+
+  const [result, setResult] = useState(0);
+  const fetchSum = async (a, b) => {
+    const response = await axios.get(`http://localhost:4000/a5/add/${a}/${b}`);
+    setResult(response.data);
+  };
+  const fetchSubtraction = async (a, b) => {
+    const response = await axios.get(
+      `http://localhost:4000/a5/subtract/${a}/${b}`
+    );
+    setResult(response.data);
+  };
+
+  useEffect(() => {
+    fetchWelcome();
+  }, []);
+
   return (
     <div>
       <h3>Encoding Parameters In URLs</h3>
+      <h4>Integrating React with APIs</h4>
+      <h5>Fetching Welcome</h5>
+      <h6>{welcome}</h6>
+
       <h4>Calculator</h4>
       <input
         className="form-control"
@@ -18,6 +47,26 @@ function EncodingParametersInURLs() {
         value={b}
         onChange={(e) => setB(e.target.value)}
       />
+      <input
+        value={result}
+        className="form-control mb-2"
+        type="number"
+        readOnly
+      />
+      <h3>Fetch Result</h3>
+      <button
+        onClick={() => fetchSum(a, b)}
+        className="btn btn-light mb-2  w-100"
+      >
+        Fetch Sum of {a} + {b}
+      </button>
+      <button
+        onClick={() => fetchSubtraction(a, b)}
+        className="btn btn-dark me-2 w-100"
+      >
+        Fetch Substraction of {a} - {b}
+      </button>
+
       <h3>Query Parameters</h3>
       <a
         href={`http://localhost:4000/a5/calculator?a=${a}&b=${b}&operation=add`}
--- src/Labs/a5/index.js
+++ src/Labs/a5/index.js
@@ -1,18 +1,20 @@
-// Import necessary libraries
 import React from "react";
 import EncodeParametersInURLs from "./EncodingParametersInURLs";
 import WorkingWithObjects from "./WorkingWithObjects";
+import WorkingWithArrays from "./WorkingWithArrays";
+
 function Assignment5() {
   return (
     <div>
       <h1>Assignment 5</h1>
-      <WorkingWithObjects />
       <EncodeParametersInURLs />
+      <WorkingWithObjects />
       <div className="list-group">
         <a href="http://localhost:4000/a5/welcome" className="list-group-item">
           Welcome
         </a>
       </div>
+      <WorkingWithArrays />
     </div>
   );
 }
